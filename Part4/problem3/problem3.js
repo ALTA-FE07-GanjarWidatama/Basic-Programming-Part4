8217b8cfb74d82abe6cea2f5f4fc53c922177a30
Ganjar Widatama <gwidatama@gmail.com>
2022-07-03 22:42:59 +0700

    salah kerangka repo wkwk

--- Part4/problem3/problem3.js
+++ Part4/problem3/problem3.js
@@ -1,5 +1,54 @@
-function joinArrayRemoveDuplicate(arrayA, arrayB) {
-  // Your Code Here
-}
+// Buatlah sebuah program menggabungkan 2 array input, dalam problem ini jika terdapat nama
+// yang sama di dalam 2 array tersebut,maka kamu hanya boleh memasukan satu nilai saja.
 
-module.exports = joinArrayRemoveDuplicate;
+// Sample Test Cases
+// Input: ['apel', 'nanas', 'anggur'], ['apel', 'pisang']
+// Output: ['apel', 'nanas', 'anggur', 'pisang']
+
+// Input: ['apel', 'nanas'], ['anggur', 'apel']
+// Output: ['apel', 'nanas', 'anggur', 'apel']
+
+var array = ["apel", "anggur", "lemon", "leci", "nanas"];
+
+function joinArrayRemoveDuplicate(arrayA) {
+  var joinArrayRemoveDuplicate = [];
+
+  for (var i = 0; i < arrayA.length; i++) {
+    if (joinArrayRemoveDuplicate.indexOf(arrayA[i]) === -1) {
+      joinArrayRemoveDuplicate.push(arrayA[i]);
+    }
+  }
+  return joinArrayRemoveDuplicate;
+}
+var joinArrayRemove = joinArrayRemoveDuplicate(array);
+console.log(joinArrayRemove);
+
+var array2 = ["samsung", "apple", "apple", "sony", "xiaomi"];
+
+function joinArrayRemoveDuplicate(arrayA) {
+  var joinArrayRemoveDuplicate = [];
+
+  for (var i = 0; i < arrayA.length; i++) {
+    if (joinArrayRemoveDuplicate.indexOf(arrayA[i]) === -1) {
+      joinArrayRemoveDuplicate.push(arrayA[i]);
+    }
+  }
+  return joinArrayRemoveDuplicate;
+}
+var joinArrayRemove = joinArrayRemoveDuplicate(array2);
+console.log(joinArrayRemove);
+
+var array3 = (["football", "basketball"], ["basketball", "football"]);
+
+function joinArrayRemoveDuplicate(arrayA) {
+  var joinArrayRemoveDuplicate = [];
+
+  for (var i = 0; i < arrayA.length; i++) {
+    if (joinArrayRemoveDuplicate.indexOf(arrayA[i]) === -1) {
+      joinArrayRemoveDuplicate.push(arrayA[i]);
+    }
+  }
+  return joinArrayRemoveDuplicate;
+}
+var joinArrayRemove = joinArrayRemoveDuplicate(array3);
+console.log(joinArrayRemove);
